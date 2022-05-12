3e8f72247625ea4cebb4aa558a1bf3739a2752d5
Artyom <73700020+ArtyomBY@users.noreply.github.com>
2022-05-12 18:24:13 +0300

    mod chats & recommendation

--- app/src/main/java/com/maxart/appforcats/fragments/ChatsFragment.java
+++ app/src/main/java/com/maxart/appforcats/fragments/ChatsFragment.java
@@ -1,7 +1,10 @@
 package com.maxart.appforcats.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
@@ -9,29 +12,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.maxart.appforcats.Chat;
 import com.maxart.appforcats.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ChatsFragment extends Fragment {
 
-    ListView chats_list;
+    ListView chats_listView;
     SearchView searchView;
     final String[] chats_array = {"Кот Валера", "Кошка Маша", "Кот Аркадий", "Беседа ..."};
     final ArrayList<String> chats = new ArrayList<>();
+    List<Chat> chats_list = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_chats, container, false);
-        chats_list = v.findViewById(R.id.chats_list);
+        chats_listView = v.findViewById(R.id.chats_list);
         searchView = v.findViewById(R.id.search_view);
+
+        chats_list.add(new Chat("Кот Аркадий"));
+        chats_list.add(new Chat("Кошка Маша"));
+        chats_list.add(new Chat("Кот Борис"));
+        chats_list.add(new Chat("Беседа ..."));
+
+        ChatsAdapter chatsAdapter = new ChatsAdapter(getContext(), R.layout.layout_chat_item, chats_list);
+        chats_listView.setAdapter(chatsAdapter);
+
+        /*
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, chats_array);
-        chats_list.setAdapter(adapter);
+        chats_listView.setAdapter(adapter);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
                     @Override
@@ -52,8 +70,35 @@ public class ChatsFragment extends Fragment {
                         return false;
                     }
                 });
+                */
 
 
         return v;
+    }
+
+
+    private class ChatsAdapter extends ArrayAdapter<Chat>{
+
+        public ChatsAdapter(@NonNull Context context, int resource, @NonNull List<Chat> objects) {
+            super(context, resource, objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            final Chat chat = getItem(position);
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_chat_item, null);
+            }
+
+            ((ImageView) convertView.findViewById(R.id.profile_picture)).setImageResource(chat.getProfile_picture());
+            ((TextView) convertView.findViewById(R.id.profile_name)).setText(chat.getName());
+
+
+
+            return convertView;
+        }
     }
 }
--- app/src/main/java/com/maxart/appforcats/fragments/RecommendationFragment.java
+++ app/src/main/java/com/maxart/appforcats/fragments/RecommendationFragment.java
@@ -1,5 +1,7 @@
 package com.maxart.appforcats.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,6 +27,44 @@ public class RecommendationFragment extends Fragment {
         recommendations_list = v.findViewById(R.id.recommendations_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, recommendations);
         recommendations_list.setAdapter(adapter);
+
+        recommendations_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String title = adapterView.getItemAtPosition(position).toString();
+                String url;
+
+
+
+                if (title.equals("Правила дрессировки")){
+                    url = "https://zen.yandex.ru/media/supercat/7-prostyh-pravil-kak-dressirovat-koshku-60a81281f264e45e6c5c8b52";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+                else if (title.equals("Советы выбора игрушек")){
+                    url = "https://sestratk.livejournal.com/431539.html";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+                else if (title.equals("Дневник настроения кота")){
+                    url = "https://www.purinaone.ru/cat/catmag/psychology/the-mood-of-the-cats";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+                else if (title.equals("Инструкции самодельных игрушек")){
+                    url = "https://zen.yandex.ru/media/snowdance/top-20-igrushek-dlia-koshek-svoimi-rukami-koshka-budet-v-vostorge-607782eaf63f032f258fda6b";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+
+
+
+            }
+        });
 
         return v;
     }
